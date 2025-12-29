d63dbe6159154cb86ee63cc2a164af1194bbf186
Michael Claesson <michael.claesson@consid.se>
2025-12-29 15:59:42 +0100

    News styling

--- source/js/main.js
+++ source/js/main.js
@@ -1,19 +1,1 @@
-/**
- * Pitea Customisation - Frontend JavaScript
- *
- * This file is the main entry point for frontend JavaScript.
- * Import your modules and initialize them here.
- */
 
-// Import styles
-import '../sass/style.scss';
-
-// Example module import
-// import { myModule } from './modules/myModule';
-
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Pitea Customisation loaded');
-
-    // Initialize your modules here
-    // myModule.init();
-});
--- source/php/Customisations/Config.php
+++ source/php/Customisations/Config.php
@@ -14,6 +14,9 @@ class Config
 
         // Specifiy Font Awesome archive link icon for service information
         add_filter('Modularity/ServiceInformation/Module/ArchiveLink/Icon', [$this, 'setServiceInfoArchiveLinkIcon']);
+
+        // Remove image from news item when used in posts module
+        add_filter('ComponentLibrary/Component/NewsItem/Data', [$this, 'removeImageFromNewsItem']);
     }
 
     /**
@@ -30,5 +33,22 @@ class Config
     public function setServiceInfoArchiveLinkIcon(): string
     {
         return 'fa-solid fa-arrow-right';
+    }
+
+    /**
+     * Remove image from news item when used in posts module
+     *
+     * @param array $data
+     * @return array
+     */
+    public function removeImageFromNewsItem(array $data): array
+    {
+        if (!in_array('module.posts.news-item', $data['context'])) {
+            return $data;
+        }
+
+        $data['image'] = null;
+
+        return $data;
     }
 }
