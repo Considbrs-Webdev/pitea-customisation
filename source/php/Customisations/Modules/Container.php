6bcc8de1d7200bdc22e9dfe010389b83c54b4b78
William Lundqvist <wille1000000@gmail.com>
2026-01-15 15:33:12 +0100

    remove spacing on full width containers

--- source/php/Customisations/Modules/Container.php
+++ source/php/Customisations/Modules/Container.php
@@ -9,29 +9,41 @@ class Container
      */
     public function __construct()
     {
-        add_filter('Municipio/Block/Container/contentClassList', [$this, 'removeContainerSpacingClass'], 10, 3);
+        // Use render_block filter to intercept Container block output
+        add_filter('render_block', [$this, 'modifyContainerBlockOutput'], 10, 2);
     }
 
     /**
-     * Remove o-container--remove-spacing class from container block
-     * Only keeps it if the container is set to full width
      *
-     * @param string $classList The content class list
-     * @param array $data The block data
+     * @param string $blockContent The rendered block content
      * @param array $block The block array
-     * @return string Modified class list
+     * @return string Modified block content
      */
-    public function removeContainerSpacingClass(string $classList, array $data, array $block): string
+    public function modifyContainerBlockOutput(string $blockContent, array $block): string
     {
-        // Only keep o-container--remove-spacing if block is full width
-        $isFullWidth = isset($block['align']) && $block['align'] === 'full';
+        // Only process Container blocks
+        if (!isset($block['blockName']) || $block['blockName'] !== 'acf/container') {
+            return $blockContent;
+        }
+        // Check if block is full width
+        $isFullWidth = isset($block['attrs']['align']) && $block['attrs']['align'] === 'full';
 
+        // Only remove o-container--remove-spacing if block is full width
         if ($isFullWidth) {
-            // Remove the class if not full width
-            return str_replace('o-container--remove-spacing', '', $classList);
+            $blockContent = preg_replace_callback(
+                '/(<div[^>]*class=["\'])([^"\']*)(["\'])/',
+                function ($matches) {
+                    $classes = preg_split('/\s+/', trim($matches[2]));
+                    $classes = array_filter($classes, function ($class) {
+                        return $class !== 'o-container--remove-spacing';
+                    });
+                    $newClasses = implode(' ', $classes);
+                    return $matches[1] . $newClasses . $matches[3];
+                },
+                $blockContent
+            );
         }
 
-        // Keep the class if full width
-        return $classList;
+        return $blockContent;
     }
 }
